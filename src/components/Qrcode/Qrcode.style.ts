aecaeabcaeea8d231993479e1c21815f1b68b430
dlittig <david0605.littig@hotmail.de>
2022-02-13 16:18:25 +0100

    style: 💄 Improved styling of the qr code

---
+++ src/components/Qrcode/Qrcode.style.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 3,
+    alignItems: "center",
+    marginVertical: 16,
+  },
+});
+
+export default styles;
--- src/components/Qrcode/Qrcode.tsx
+++ src/components/Qrcode/Qrcode.tsx
@@ -1,9 +1,14 @@
 import { FC } from "react";
+import { Dimensions, View } from "react-native";
 import QRCode from "react-native-qrcode-svg";
 import { QrcodeComponentType } from "./types";
 
+import style from "./Qrcode.style";
+
 const Qrcode: FC<QrcodeComponentType> = ({ value }) => (
-  <QRCode value={value} size={50} />
+  <View style={style.container}>
+    <QRCode value={value} size={Dimensions.get("window").width * 0.6} />
+  </View>
 );
 
 export default Qrcode;
